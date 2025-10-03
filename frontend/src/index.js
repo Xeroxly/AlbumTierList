e2c4680cd9d9f333b7f7b2d636950fb2bf9c6987
Trevor Sloan <trevor.sloan1@hotmail.com>
2025-10-03 11:02:52 -0600

    remove <StrictMode>

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { StrictMode } from "react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+root.render(<App />);
